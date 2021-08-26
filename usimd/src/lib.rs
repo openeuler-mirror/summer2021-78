906e977baa614b1219da3909c6e941507b49748b
fanbo <3374614481@qq.com>
2021-08-26 21:44:58 +0800

    ,

--- usimd/src/lib.rs
+++ usimd/src/lib.rs
@@ -7,13 +7,17 @@ mod tests {
 
     #[test]
     fn add_works() {
-        let a = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
-        let b = vec![16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
-        let a_usimd = USimdI32::from(&a);
-        let b_usimd = USimdI32::from(&b);
-        let c = a_usimd + b_usimd;
+        let mut a = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+        let mut b = vec![16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
+        let a_usimd = USimdI32::from(& mut a);
+        let b_usimd = USimdI32::from(& mut b);
+        let c = &a_usimd + &b_usimd;
         for i in 0..16 {
             assert_eq!(c[i],17);
+        }
+        let d = a_usimd + b_usimd;
+        for i in 0..16 {
+            assert_eq!(d.data[i],17);
         }
     }
 }
--- usimd/src/usimd_type.rs
+++ usimd/src/usimd_type.rs
@@ -2,29 +2,31 @@ use std::arch::x86_64::*;
 use std::ops::Add;
 
 pub struct USimdI32<'a> {
-    pub data: &'a Vec<i32>,
+    pub data: &'a mut Vec<i32>,
 }
 
-impl<'a> From<&'a Vec<i32>> for USimdI32<'a> {
-    fn from(item: &'a Vec<i32>) -> Self {
+impl<'a> From<&'a  mut Vec<i32>> for USimdI32<'a> {
+    fn from(item: &'a mut Vec<i32>) -> Self {
         USimdI32 { data: item }
     }
 }
 
-impl<'a> Add for USimdI32<'a> {
+impl<'a> Add for &USimdI32<'a> {
     type Output = Vec<i32>;
 
     fn add(self, rhs: Self) -> Self::Output {
         return if is_x86_feature_detected!("avx2") {
-            unsafe { add_avx2(self, rhs) }
+            unsafe { add_avx2_ref(self, rhs) }
         } else {
-            unsafe { add_avx2(self, rhs) }
+            unsafe { add_avx2_ref(self, rhs) }
         };
     }
 }
 
+
+
 #[target_feature(enable = "avx2")]
-unsafe fn add_avx2<'a>(a: USimdI32<'a>, b: USimdI32<'a>) -> Vec<i32> {
+unsafe fn add_avx2_ref<'a>(a: &USimdI32<'a>, b: &USimdI32<'a>) -> Vec<i32> {
     let len_a = a.data.len();
     let len_b = b.data.len();
     if len_a != len_b {
@@ -46,4 +48,41 @@ unsafe fn add_avx2<'a>(a: USimdI32<'a>, b: USimdI32<'a>) -> Vec<i32> {
         result[group * 8 + i] = a.data[group * 8 + i] + b.data[group * 8 + i];
     }
     result
+}
+
+impl<'a> Add for USimdI32<'a> {
+    type Output = Self;
+
+    fn add(self, rhs:Self) -> Self::Output {
+        return if is_x86_feature_detected!("avx2") {
+            unsafe { add_avx2(self, rhs) }
+        } else {
+            unsafe { add_avx2(self, rhs) }
+        };
+    }
+}
+
+
+
+#[target_feature(enable = "avx2")]
+unsafe fn add_avx2<'a>(a:USimdI32<'a>, b: USimdI32<'a>) -> USimdI32<'a> {
+    let len_a = a.data.len();
+    let len_b = b.data.len();
+    if len_a != len_b {
+        panic!("Vectors can't be added because the lengths are unequal");
+    }
+
+    let group = len_a / 8;
+    for i in 0..group {
+        let a_vector = _mm256_loadu_epi32(&a.data[i * 8] as *const i32);
+        let b_vector = _mm256_loadu_epi32(&b.data[i * 8] as *const i32);
+        _mm256_store_epi32(
+            &mut a.data[i * 8] as *mut i32,
+            _mm256_add_epi32(a_vector, b_vector),
+        );
+    }
+    for i in 0..len_a - group * 8 {
+        a.data[group * 8 + i] = a.data[group * 8 + i] + b.data[group * 8 + i];
+    }
+    USimdI32::from(a.data)
 }
